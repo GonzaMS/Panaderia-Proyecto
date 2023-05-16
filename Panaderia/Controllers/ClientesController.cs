2fad9fff024701c6c1d8d1ebd394cc0931cafa18
jorcandia <jorcandia03@gmail.com>
2023-05-15 21:39:45 -0400

    api clientes

---
+++ Panaderia/Controllers/ClientesController.cs
@@ -0,0 +1,98 @@
+using Panaderia.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Panaderia.Data;
+
+namespace Panaderia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ClientesController : ControllerBase
+    {
+        private readonly ClientesDbContext _context;
+
+        public ClientesController(ClientesDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Clientes>>> GetClientes()
+        {
+            return await _context.Clientes.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Clientes>> GetClientes(int id)
+        {
+            var clientes = await _context.Clientes.FindAsync(id);
+
+            if (clientes == null)
+            {
+                return NotFound();
+            }
+
+            return clientes;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutClientes(int id, Clientes clientes)
+        {
+            if (id != clientes.id_cliente)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(clientes).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClientesExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Clientes>> PostClientes(Clientes clientes)
+        {
+            _context.Clientes.Add(clientes);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetClientes", new { id = clientes.id_cliente }, clientes);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Clientes>> DeleteClientes(int id)
+        {
+            var clientes = await _context.Clientes.FindAsync(id);
+            if (clientes == null)
+            {
+                return NotFound();
+            }
+
+            _context.Clientes.Remove(clientes);
+            await _context.SaveChangesAsync();
+
+            return clientes;
+        }
+
+        private bool ClientesExists(int id)
+        {
+            return _context.Clientes.Any(e => e.id_cliente == id);
+        }
+
+    }
+}
